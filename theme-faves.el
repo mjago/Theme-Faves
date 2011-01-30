a8be41866246a48c17599d4225fc974a41cf9eef
Martyn Jago <martyn.jago@btinternet.com>
2011-01-30 20:25:23 +0000

    theme-faves-insert fixed for either mode operation

--- theme-faves.el
+++ theme-faves.el
@@ -9,39 +9,40 @@
   (setq theme-faves-saved-idx 0)
   (theme-faves-activate) t)
 
-(defun theme-get-fave-full-idx(idx)
+(defun theme-faves-get-fave-full-idx(idx)
   (nth idx theme-faves-list))
 
-(defun theme-get-fave-theme(idx)
-  (let ((temp (theme-get-fave-full-idx idx)))
-    (when temp (nth temp theme-faves-audition-list)
-          nil)))
+(defun theme-faves-get-fave-theme(idx)
+  (let ((temp (theme-faves-get-fave-full-idx idx)))
+    (nth temp theme-faves-audition-list)))
 
-(defun theme-faves-insert (elem org-list pos)
+(defun theme-faves--insert (elem org-list pos)
   (if (or (equal pos 0)
           (equal org-list nil))
       (cons elem org-list)
-    (cons (car org-list) (theme-faves-insert elem (cdr org-list) (- pos 1)))))
+    (cons (car org-list) (theme-faves--insert elem (cdr org-list) (- pos 1)))))
 
-(defun theme-faves-save ()
-  "Save current auditioned theme to favourites"
+(defun theme-faves-insert ()
+  "Insert current auditioned theme into favourites"
   (interactive)
   (theme-faves-startup-check)
   (when theme-faves-active-idx
     (progn
+      (when (not theme-faves-has-focus-p)
+          (theme-faves-cycle-up))
       (customize-save-variable
        'theme-faves-list
-       (theme-faves-insert
-        theme-faves-active-idx
+       (theme-faves--insert
+        theme-faves-saved-idx
         theme-faves-list
-        (if theme-faves-has-focus-p
-            theme-faves-active-idx
-          theme-faves-saved-idx)))
-      (theme-faves-saved-message))))
+        theme-faves-active-idx))
+      (theme-faves-activate)
+      (theme-faves-insert-message)
+      )))
 
-(defun theme-faves-saved-message ()
-  (message "%S added to favourite themes"
-           (nth theme-faves-active-idx  theme-faves-audition-list)))
+(defun theme-faves-insert-message ()
+  (message "%S inserted into favourites"
+          (theme-faves-get-fave-theme theme-faves-active-idx)))
 
 (defun theme-faves-remove (org-list pos &optional ini)
   (unless ini
@@ -54,15 +55,18 @@
   "Delete current favourite from favourites. You can add back to favourites by re-auditioning again later if you need to."
   (interactive)
   (theme-faves-startup-check)
-  (when (and theme-faves-has-focus-p
-             theme-faves-active-idx
+  (when (not theme-faves-has-focus-p)
+    (theme-faves-cycle-up))
+
+  (when (and theme-faves-active-idx
              (> (length theme-faves-list) 1))
     (progn
       (customize-save-variable
        'theme-faves-list
        (theme-faves-remove theme-faves-list
                            (+ 1 theme-faves-active-idx)))
-      (theme-faves--cycle t))))
+      (theme-faves-activate)
+      )))
 
 ;;TODO
 (defun theme-faves-delete-audition ()
@@ -91,7 +95,7 @@
 (defun theme-faves--cycle (do-faves-p direction)
   (interactive)
   (if (not (equal do-faves-p theme-faves-has-focus-p))
-      (theme-faves-swap-cycle-mode do-faves-p direction)
+      (theme-faves-swap-cycle-mode do-faves-p)
     (theme-faves-next-theme direction))
   (theme-faves-activate))
 
@@ -111,10 +115,9 @@
   (if(not (boundp 'theme-faves-saved-idx))
       (progn
         (theme-faves-init)
-        nil)
-    t))
+        nil) t))
 
-(defun theme-faves-swap-cycle-mode (do-faves-p direction)
+(defun theme-faves-swap-cycle-mode (do-faves-p)
   (let ((temp theme-faves-active-idx))
     (setq theme-faves-active-idx theme-faves-saved-idx)
     (setq theme-faves-saved-idx temp)
@@ -178,6 +181,12 @@
            (if theme-faves-has-focus-p
                (length theme-faves-list)
              (length theme-faves-audition-list))))
+
+(defun theme-faves-debug-message ()
+  (message "theme-faves-active-idx: %d - theme-faves-saved-idx: %d - theme-faves-has-focus-p: %S"
+           theme-faves-active-idx
+           theme-faves-saved-idx
+           theme-faves-has-focus-p))
 
 (defcustom theme-faves-idx 0
   "Index of favourite in use"
@@ -351,162 +360,3 @@
   :group 'theme-faves)
 
 (provide 'theme-faves)
-
-( customize-save-variable
-  'theme-faves-audition-list
-  (list
-   'color-theme-renegade
-   'color-theme-feng-shui
-   'color-theme-matrix
-   'color-theme-lawrence
-   'color-theme-calm-forest
-   'color-theme-vim-colors
-   'color-theme-charcoal-black
-   'color-theme-andreas
-   'color-theme-clarity
-   'color-theme-late-night
-   'color-theme-emacs-nw
-   'color-theme-shaman
-   'color-theme-lethe
-   'color-theme-bharadwaj-slat
-   'color-theme-whateveryouwant
-   'color-theme-dark-green
-   'color-theme-gray30
-   'color-theme-xp
-   'color-theme-resolve
-   'color-theme-euphoria
-   'color-theme-blue-mood
-   'color-theme-dark-blue2
-   'color-theme-black-on-gray
-   'color-theme-aliceblue
-   'color-theme-tty-dark
-   'color-theme-arjen
-   'color-theme-katester
-   'color-theme-comidia
-   'color-theme-kingsajz
-   'color-theme-deep-blue
-   'color-theme-ld-dark
-   'color-theme-jsc-light2
-   'color-theme-emacs-21
-   'color-theme-word-perfect
-   'color-theme-gray1
-   'color-theme-jonadabian-slate
-   'color-theme-dark-blue
-   'color-theme-subtle-blue
-   'color-theme-dark-erc
-   'color-theme-blue-erc
-   'color-theme-marine
-   'color-theme-mistyday
-   'color-theme-digital-ofs1
-   'color-theme-taming-mr-arneson
-   'color-theme-dark-laptop
-   'color-theme-snowish
-   'color-theme-robin-hood
-   'color-theme-salmon-diff
-   'color-theme-oswald
-   'color-theme-bharadwaj
-   'color-theme-hober
-   'color-theme-blippblopp
-   'color-theme-aalto-dark
-   'color-theme-aalto-light
-   'color-theme-montz
-   'color-theme-snow
-   'color-theme-jedit-grey
-   'color-theme-midnight
-   'color-theme-gtk-ide
-   'color-theme-scintilla
-   'color-theme-classic
-   'color-theme-infodoc
-   'color-theme-high-contrast
-   'color-theme-parus
-   'color-theme-marquardt
-   'color-theme-taylor
-   'color-theme-raspopovic
-   'color-theme-ramangalahy
-   'color-theme-goldenrod
-   'color-theme-beige-eshell
-   'color-theme-standard-ediff
-   'color-theme-beige-diff
-   'color-theme-jb-simple
-   'color-theme-greiner
-   'color-theme-jsc-dark
-   'color-theme-jsc-light
-   'color-theme-xemacs
-   'color-theme-pierson
-   'color-theme-rotor
-   'color-theme-blue-sea
-   'color-theme-pok-wob
-   'color-theme-pok-wog
-   'color-theme-subtle-hacker
-   'color-theme-retro-orange
-   'color-theme-retro-green
-   'color-theme-billw
-   'color-theme-sitaramv-nt
-   'color-theme-sitaramv-solaris
-   'color-theme-fischmeister
-   'color-theme-standard
-   'color-theme-wheat
-   'color-theme-ryerson
-   'color-theme-jonadabian
-   'color-theme-simple-1
-   'color-theme-gnome2
-   'color-theme-dark-info
-   'color-theme-dark-font-lock
-   'color-theme-salmon-font-lock
-   'color-theme-blue-eshell
-   'color-theme-dark-gnus
-   'color-theme-blue-gnus
-   'color-theme-gnome
-   'color-theme-hober
-   'color-theme-emacs-21
-   'color-theme-arjen
-   'color-theme-bharadwaj-slate
-   'color-theme-billw
-   'color-theme-blue-gnus
-   'color-theme-dark-gnus
-   'color-theme-blue-eshell
-   'color-theme-retro-green
-   'color-theme-retro-orange
-   'color-theme-subtle-hacker
-   'color-theme-salmon-font-lock
-   'color-theme-dark-font-lock
-   'color-theme-dark-info
-   'color-theme-simple-1
-   'color-theme-jonadabian
-   'color-theme-ryerson
-   'color-theme-standard
-   'color-theme-blue-mood
-   'color-theme-sitaramv-solaris
-   'color-theme-sitaramv-nt
-   'color-theme-blue-sea
-   'color-theme-calm-forest
-   'color-theme-charcoal-black
-   'color-theme-clarity
-   'color-theme-classic
-   'color-theme-comidia
-   'color-theme-dark-blue
-   'color-theme-dark-blue2
-   'color-theme-dark-erc
-   'color-theme-dark-laptop
-   'color-theme-deep-blue
-   'color-theme-digital-ofs1
-   'color-theme-emacs-nw
-   'color-theme-euphoria
-   'color-theme-feng-shui
-   'color-theme-fischmeister
-   'color-theme-gnome
-   'color-theme-gnome2
-   'color-theme-goldenrod
-   'color-theme-gray1
-   'color-theme-gray30
-   'color-theme-greiner
-   'color-theme-infodoc
-   'color-theme-lawrence
-   'color-theme-ld-dark
-   'color-theme-montz
-   'color-theme-oswald
-   'color-theme-pok-wob
-   'color-theme-pok-wog
-   'color-theme-tty-dark
-   'color-theme-xp))
-
